0d7c148890f9be000ea2472d8c0bdde15bb23f5c
Marvin <okongomarvin971@gmail.com>
2024-06-03 22:15:30 +0300

    Completed

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="all">
+      <div className="lefter">
+        <div className="text">Hosting</div>
+      </div>
+      <div className="left">
+        <div className="text">Web Design</div>
+      </div>
+      <div className="center">
+        <div className="explainer"><span>Hover me</span></div>
+        <div className="text">Frontend Development</div>
+      </div>
+      <div className="right">
+        <div className="text">Backend Development</div>
+      </div>
+      <div className="righter">
+        <div className="text">SEO</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
